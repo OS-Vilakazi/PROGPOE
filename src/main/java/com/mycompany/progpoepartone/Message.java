6dca37dbbc197cf49015cff31738d12e71b9957b
Olwethu Vilakazi <st10537650@rcconnect.edu.za>
2026-05-23 20:40:28 +0200

    i added  import java.util.ArrayList import java.util.Random
    Without these imports, Java would throw a compile error — it simply wouldn't know what ArrayList or Random refer to.

--- src/main/java/com/mycompany/progpoepartone/Message.java
+++ src/main/java/com/mycompany/progpoepartone/Message.java
@@ -3,7 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.progpoepartone;
-
+//to have resizable array 
+import java.util.ArrayList;
+//Random class is used to generate pseudo-random numbers in java
+import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author falconsyre
